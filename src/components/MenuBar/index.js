fd8d4116f4e2ee7edc858b4b82c823777c90458a
Felipe Luna <felipebluna@hotmail.com>
2020-03-09 15:25:51 -0300

    ajustndo darklight no botao

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect}from "react"
 import { Home } from "styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
 import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
@@ -8,7 +8,17 @@ import { Grid } from "styled-icons/boxicons-solid/Grid"
 
 import * as S from './styled'
 
-const MenuBar = () => (
+const MenuBar = () => {
+    const [theme, setTheme] = useState(null)
+
+    const isDarkMode = theme === 'dark'
+
+    useEffect(() => {
+        setTheme(window.__theme)
+        window.__onThemeChange = () => setTheme(window.__theme)
+    }, [])
+
+    return (
     <S.MenuBarWrapper>
         <S.MenuBarGroup>
             <S.MenuBarLink to="/" tittle="Voltar para Home">
@@ -24,10 +34,16 @@ const MenuBar = () => (
         </S.MenuBarGroup>
 
         <S.MenuBarGroup>
-            <S.MenuBarItem title="Mudar o tema">
+            <S.MenuBarItem 
+            title="Mudar o tema"
+            onClick={() => {
+                window.__setPreferredTheme(isDarkMode ? "light" : "dark")
+            }}
+            className={theme}
+            >
                <Light />
-                </S.MenuBarItem>
-            <S.MenuBarItem title="Mudar Visualização">
+            </S.MenuBarItem>
+            <S.MenuBarItem title="Mudar visualização">
                 <Grid />
                 </S.MenuBarItem>
             <S.MenuBarItem title="Ir para o topo">
@@ -36,8 +52,8 @@ const MenuBar = () => (
         </S.MenuBarGroup>
 
     </S.MenuBarWrapper>
-
-)
+    )
+}
 
 
 export default MenuBar
--- src/styles/global.js
+++ src/styles/global.js
@@ -62,7 +62,7 @@ const GlobalStyles = createGlobalStyle`
   	width: 100%;
   	height: auto;
   }
-  body {
+  body.dark {
     --borders: #38444d;
     --texts: #8899a6;
     --postColor: #fff;
