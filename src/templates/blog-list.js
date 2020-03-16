603a222757e6bd7a5d7755525f1b2e2f8554ec61
Felipe Luna <felipebluna@hotmail.com>
2020-03-16 09:37:25 -0300

    arrumando o grid

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -4,41 +4,45 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import PostItem from "../components/PostItem"
 import SEO from "../components/seo"
-import Pagination from '../components/Pagination'
+import Pagination from "../components/Pagination"
+
+import * as S from "../components/ListWrapper/styled"
 
 const BlogList = props => {
-    const postList = props.data.allMarkdownRemark.edges
+  const postList = props.data.allMarkdownRemark.edges
 
-    const { currentPage, numPages } = props.pageContext
-    const isFirst = currentPage === 1
-    const isLast = currentPage === numPages
-    const prevPage = currentPage - 1 === 1 ? '/' : `/page/${currentPage - 1}`
-    const nextPage = `/page/${currentPage + 1}`
+  const { currentPage, numPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? "/" : `/page/${currentPage - 1}`
+  const nextPage = `/page/${currentPage + 1}`
 
 
-    return (
-        <Layout>
-            <SEO title="Home" />
-            {postList.map(
-                ({
-                    node: {
-                        frontmatter: { background, category, date, description, title },
-                        timeToRead,
-                        fields: { slug }
-                    },
-                }) => (
-                        <PostItem
-                            slug={slug}
-                            background={background}
-                            category={category}
-                            date={date}
-                            timeToRead={timeToRead}
-                            title={title}
-                            description={description}
-                        />
-                    )
-            )}
-            
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <S.ListWrapper>
+        {postList.map(
+          ({
+            node: {
+              frontmatter: { background, category, date, description, title },
+              timeToRead,
+              fields: { slug },
+            },
+          }) => (
+              <PostItem
+                slug={slug}
+                background={background}
+                category={category}
+                date={date}
+                timeToRead={timeToRead}
+                title={title}
+                description={description}
+              />
+            )
+        )}
+      </S.ListWrapper>
+
       <Pagination
         isFirst={isFirst}
         isLast={isLast}
@@ -47,8 +51,8 @@ const BlogList = props => {
         prevPage={prevPage}
         nextPage={nextPage}
       />
-        </Layout>
-    )
+    </Layout>
+  )
 }
 
 export const query = graphql`
